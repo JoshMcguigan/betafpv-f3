b3de9e5ce3c6d0e1507eaa40a399bfe20710f805
Josh Mcguigan <joshmcg88@gmail.com>
2018-07-15 15:47:33 -0700

    Expose LED through board struct

--- examples/led-control.rs
+++ examples/led-control.rs
@@ -8,22 +8,16 @@ extern crate cortex_m_rt as rt;
 extern crate panic_semihosting;
 
 use betafpv_f3::hal::prelude::*;
-use betafpv_f3::hal::stm32f30x;
+use betafpv_f3::hal::stm32f30x::Peripherals;
+use betafpv_f3::Board;
 use cortex_m::asm::nop;
 use rt::ExceptionFrame;
 
 entry!(main);
 
 fn main() -> ! {
-    let p = stm32f30x::Peripherals::take().unwrap();
-    let mut rcc = p.RCC.constrain();
 
-
-    let mut gpioc = p.GPIOC.split(&mut rcc.ahb);
-
-    let mut led = gpioc
-        .pc15
-        .into_push_pull_output(&mut gpioc.moder, &mut gpioc.otyper);
+    let Board {mut led} = Board::new(Peripherals::take().unwrap());
 
     loop {
         led.set_high();
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,28 @@
 #![no_std]
 pub extern crate stm32f30x_hal as hal;
 
+use hal::gpio::{Output, PushPull};
+use hal::gpio::gpioc::PC15;
+
+use hal::prelude::*;
+use hal::stm32f30x::Peripherals;
+
+pub struct Board {
+    pub led: PC15<Output<PushPull>>
+}
+
+impl Board {
+    pub fn new(p: Peripherals) -> Self {
+        let mut rcc = p.RCC.constrain();
+
+        let mut _gpioa = p.GPIOA.split(&mut rcc.ahb);
+        let mut _gpiob = p.GPIOB.split(&mut rcc.ahb);
+        let mut gpioc = p.GPIOC.split(&mut rcc.ahb);
+
+        let mut led = gpioc
+            .pc15
+            .into_push_pull_output(&mut gpioc.moder, &mut gpioc.otyper);
+
+        Board { led }
+    }
+}
